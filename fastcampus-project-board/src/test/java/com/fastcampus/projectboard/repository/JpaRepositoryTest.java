a559609bb355643160ee393576041ecce1f3f4cb
nataliekmkim <haprudal@naver.com>
2024-02-01 17:37:30 +0900

    #18 -  리팩토링: 사용 안하는 import 정리
    강의 중 optimize import 하지 않은 듯.
    정리함

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,13 +1,11 @@
 package com.fastcampus.projectboard.repository;
 
-import com.fastcampus.projectboard.FastCampusProjectBoardApplication;
 import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
