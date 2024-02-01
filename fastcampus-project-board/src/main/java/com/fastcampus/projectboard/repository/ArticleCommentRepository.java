0ce404a8c8be3f5ace830a3a96eb03465780afd5
nataliekmkim <haprudal@naver.com>
2024-02-01 18:39:41 +0900

    #18 - 게시글, 댓글 repository 수정
    누락된 부분 넣어줌.

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -11,17 +11,19 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long> ,
         QuerydslPredicateExecutor<ArticleComment>,
         QuerydslBinderCustomizer<QArticleComment>
 {
+    List<ArticleComment> findByArticle_Id(Long articleId);
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root){
         bindings.excludeUnlistedProperties(true);
         bindings.including(root.content,root.createdAt, root.createdBy);
-//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); //like '${v}'
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase); //like '%${v}%'
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
