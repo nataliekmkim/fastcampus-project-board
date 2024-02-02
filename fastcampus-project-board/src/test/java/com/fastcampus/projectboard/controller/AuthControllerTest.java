200d3731f7b4e72c9bdf16058fe6c8aec11e6061
nataliekmkim <haprudal@naver.com>
2024-02-03 02:38:31 +0900

    #22 - 예제 문자 삭제, 로그인 테스트 문제 해결
    부트스트랩 예제 코드를 사용하면서 복사된 예제 문자열 삭제
    `/login` 페이지는 직접 만든 컨트롤러가 아니고
    스프링 시큐리티가 해주는 작업이므로
    컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없음
    이를 `Void.class`로 표현

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,19 +1,18 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증")
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
     private final MockMvc mvc;
 
@@ -29,7 +28,7 @@ public class AuthControllerTest {
         //when_then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
-
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
