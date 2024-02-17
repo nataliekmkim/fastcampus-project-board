1244dfb4d12fea95e153a7616c004c589cc1b46a
nataliekmkim <haprudal@naver.com>
2024-02-18 01:03:57 +0900

    #45 - 문제가 된 테스트 코드 발견 및 수정
    이 부분은 진작에 개발 끝나고 테스트를 완료했던 코드인데,
    어느샌가 옛날 버전으로 돌아가 있다.
    정확한 원인은 알 수 없으나, 복구하고 테스트 완료.
    전체 테스트 통과함

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -1,16 +1,17 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@DisplayName("View 루트 컨트롤러")
 @Import(TestSecurityConfig.class) //이부분을 securityConfig.class로 해서 에러남
 @WebMvcTest(MainController.class)
 class MainControllerTest {
@@ -21,6 +22,7 @@ class MainControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 리스트 (게시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
@@ -29,8 +31,7 @@ class MainControllerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
     }
 
 }
