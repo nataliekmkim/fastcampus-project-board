84cc4602cc7d8f0de26128ff85a04766c899c2ee
nataliekmkim <haprudal@naver.com>
2024-02-18 00:00:37 +0900

    #35 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야
    패스워드 인코더가 읽을 수 있다.
    이에 패스워드 데이터를 알맞게 수정함

--- fastcampus-project-board/src/main/resources/data.sql
+++ fastcampus-project-board/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('kmkim', 'asdf1234','Kmkim', 'kmkim@mail.com', 'I am Kmkim.', now(),'kmkim', now(), 'kmkim')
+    ('kmkim', '{noop}asdf1234','Kmkim', 'kmkim@mail.com', 'I am Kmkim.', now(),'kmkim', now(), 'kmkim')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('kmkim2', 'asdf5678', 'Kmkim2', 'kmkim2@mail.com', 'I am Kmkim2.', now(), 'kmkim2', now(), 'kmkim2')
+    ('kmkim2', '{noop}asdf5678', 'Kmkim2', 'kmkim2@mail.com', 'I am Kmkim2.', now(), 'kmkim2', now(), 'kmkim2')
 ;
 
 
